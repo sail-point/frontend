d1e1730a44342007757aa3264a021fc57e871c39
mackoyokcam <mark.hangarter@gmail.com>
2017-11-14 10:00:19 -0800

    Better, stronger, faster? Keypad.

--- src/component/keypad/index.js
+++ src/component/keypad/index.js
@@ -1,13 +1,19 @@
 import './keypad.scss'
 import React from 'react'
+import * as util from '../../lib/util.js'
+
+let emptyState = {
+  pinCount: 0,
+  pinDirty: false,
+  pinError: 'Incorrect PIN',
+  pinClass: 'pinBox',
+}
 
 class Keypad extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      pinCount: 0,
-    }
+    this.state = emptyState
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleClick = this.handleClick.bind(this)
     this.handleCancel = this.handleCancel.bind(this)
@@ -19,7 +25,8 @@ class Keypad extends React.Component {
   }
 
   handleSubmit(e) {
-    this.setState({pinCount: 0})
+    this.setState({pinCount: 0, pinDirty: true, pinClass: 'pinBox shaker'})
+    setTimeout(() => this.setState({pinClass: 'pinBox'}), 250)
     // Dispatch action to send pin
   }
 
@@ -30,22 +37,21 @@ class Keypad extends React.Component {
       return {pinCount: prevState.pinCount+1}
     })
 
-    // Take this out when able to send pin to backend
+    // Take out setTimeout when able to send to back end
     if (this.state.pinCount === 3) {
-      this.setState({pinCount: 0})
+      setTimeout(this.handleSubmit, 250)
     }
-
-    // Dispatch action to update pin being sent
   }
 
   render(){
-    let {pinCount} = this.state
+    let {pinCount, pinDirty, pinError, pinClass} = this.state
     let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     let pin = ['0', '1', '2', '3']
     let classText
     return (
       <div className='keypad'>
-        <div className='pinBox'>
+        {util.renderIf(pinDirty, <p className='error'>{pinError}</p>)}
+        <div className={pinClass}>
           {pin.map((dot, i) => {
             classText = 'pinDot'
             if (pinCount >= Number(i)+1)
@@ -63,7 +69,7 @@ class Keypad extends React.Component {
             </button>
           })}
         </div>
-        {pinCount > 0 ? <button onClick={this.handleCancel}>Cancel</button> : undefined}
+        {util.renderIf(pinCount, <button onClick={this.handleCancel}>Cancel</button>)}
       </div>
     )
   }
