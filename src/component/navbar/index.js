681d0b4aa58eae034c98b89085522c1352f89404
Ron Barrantes <ronbarrantes@gmail.com>
2017-11-14 11:32:51 -0800

    adding a class to the navbar

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -5,7 +5,7 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <nav>
+      <nav className='navbar'>
         <ul>
           <li><Link to='/admin/employee'>Employee</Link></li>
           <li><Link to='/admin/product'>Product</Link></li>
