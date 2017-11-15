f1d382697ebb6a29bb23b1d05984528c9b1f86be
mackoyokcam <mark.hangarter@gmail.com>
2017-11-14 23:32:12 -0800

    Refactored overall product view and logic

--- src/component/product-form/index.js
+++ src/component/product-form/index.js
@@ -1,12 +1,18 @@
+import './product-form.scss'
 import React from 'react'
 import * as util from '../../lib/util.js'
 
 let emptyState = {
   name: '',
   nameDirty: false,
-  nameError: 'Name is required.',
+  nameError: 'Name is required',
   price: '',
+  priceDirty: false,
+  priceError: 'Price is required',
   category: '',
+  categoryDirty: false,
+  categoryError: '',
+  available: false,
 }
 
 class ProductForm extends React.Component {
@@ -19,25 +25,42 @@ class ProductForm extends React.Component {
   }
 
   handleChange(e){
-    let {name, value} = e.target
-    this.setState({
-      [name]: value,
-      [name+'Dirty']: true,
-      [name+'Error']: this.handleValidate(e.target),
-    })
+    let {name, value, type, checked} = e.target
+    if (type === 'checkbox') {
+      this.setState({available: checked})
+    }
+    else {
+      this.setState({
+        [name]: value,
+        [name+'Dirty']: true,
+        [name+'Error']: this.handleValidate(e.target),
+      })
+    }
   }
 
   handleSubmit(e){
     e.preventDefault()
-    this.props.onComplete(this.state)
-    this.setState(emptyState)
+
+    if (!this.state.name)
+      this.setState({nameDirty: true})
+    if (!this.state.price)
+      this.setState({priceDirty: true})
+
+    if (this.state.name && this.state.price) {
+      this.props.onComplete(this.state)
+      this.setState(emptyState)
+    }
   }
 
   handleValidate({type, placeholder, value}){
     switch(type){
       case 'text':
         if(value.length === 0)
-          return placeholder + ' is required.'
+          return placeholder + ' is required'
+        return null
+      case 'number':
+        if(value.length === 0)
+          return placeholder + ' is required'
         return null
       default:
         return null
@@ -49,48 +72,56 @@ class ProductForm extends React.Component {
       <form
         className='product-form'
         onSubmit={this.handleSubmit}>
-
-        {util.renderIf(this.state.nameDirty,
-          <p>{this.state.nameError}</p>
-        )}
-        <input
-          type='text'
-          id='name'
-          name='name'
-          value={this.state.name}
-          onChange={this.handleChange}
-          placeholder='Name'
-        />
-
-        <input
-          type='text'
-          id='price'
-          name='price'
-          value={this.state.price}
-          onChange={this.handleChange}
-          placeholder='Price'
-        />
-
-        <input
-          type='text'
-          id='category'
-          name='category'
-          value={this.state.category}
-          onChange={this.handleChange}
-          placeholder='Category'
-        />
+        <p>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder='Name'
+          />
+          <span style={{fontSize: '1.2em', color: 'red'}}>* </span>
+          {util.renderIf(this.state.nameDirty,
+            <label style={{color: 'red'}} htmlFor='name'>{this.state.nameError}</label>
+          )}
+        </p>
+        <p>
+          <input
+            type='text'
+            id='category'
+            name='category'
+            value={this.state.category}
+            onChange={this.handleChange}
+            placeholder='Category'
+          />
+        </p>
+        <p>
+          <input
+            type='number'
+            id='price'
+            name='price'
+            value={this.state.price}
+            onChange={this.handleChange}
+            placeholder='Price'
+          />
+          <span style={{fontSize: '1.2em', color: 'red'}}>* </span>
+          {util.renderIf(this.state.priceDirty,
+            <label style={{color: 'red'}} htmlFor='price'>{this.state.priceError}</label>
+          )}
+        </p>
 
         <label htmlFor='available'>Available</label>
         <input
           type='checkbox'
           id='available'
           name='available'
-          value={this.state.available}
+          checked={this.state.available}
           onChange={this.handleChange}
         />
 
         <p>
-          <button type='submit'>{this.props.product ? 'update' : 'create'} product</button>
+          <button type='submit'>{this.props.product ? 'Update' : 'Create'} </button>
         </p>
       </form>
     )
--- src/reducer/token.js
+++ src/reducer/token.js
@@ -1,8 +1,8 @@
 import { rehydrateCookie } from '../lib/redux-persist.js'
 
-let intialState = rehydrateCookie('X-SailPoint-token', null)
-
-export default (state=intialState, {type, payload}) => {
+let initialState = rehydrateCookie('X-SailPoint-token', null)
+initialState = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblNlZWQiOiIwNzQxODAwNzU1YmUzMDUwODVmNGRlMTNiNGVjMjU4ZDk4ZjkzZjA0ZDkyNGYwZjc4YjQwNWRlZWExNzc2Nzg2NjU2MDU4MjdjNWUzNTQxNjk4ZmVkYTJhNTY0OTQyM2JhMjAzMjVmZmNmNzJjODY2Y2IxNDZlZDUyNTZhYWI5NSIsImlhdCI6MTUxMDcxODg3NSwiZXhwIjoxNTExMzIzNjc1fQ.HVJ69cGP7D0UJN8TtqNu1-EHymcBq-8ysEeWMh78tJ8'
+export default (state=initialState, {type, payload}) => {
   switch(type){
     case 'TOKEN_SET':
       return payload
