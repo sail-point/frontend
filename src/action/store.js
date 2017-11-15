ee5b0acadc6b4173fc6a6564ecc709114731fe44
Christina <penssake@gmail.com>
2017-11-15 07:37:18 -0800

    auth-form

--- src/action/store.js
+++ src/action/store.js
@@ -1,5 +1,5 @@
 import superagent from 'superagent'
-import { cookieDelete } from '../lib/util,js'
+import { cookieDelete } from '../lib/util.js'
 
 export const tokenSet = (token) => ({
   type: 'TOKEN_SET',
@@ -15,22 +15,22 @@ export const signup = (user) => (store) => {
     .send(user)
     .withCredentials()
     .then(response => {
-      console.log('SIGNUP ::', { response });
+      console.log('SIGNUP ::', { response })
       return store.dispatch(tokenSet(response.body.token))
     })
-  }
+}
 
 export const login = (user) => (store) => {
-  return superagent.post(`${__API_URL__}/admin/login`) => {
+  return superagent.post(`${__API_URL__}/admin/login`)
     .auth(user.storeName, user.email, user.password)
     .withCredentials()
     .then(response => {
-      console.log('ADMIN_LOGIN ::', { response });
+      console.log('ADMIN_LOGIN ::', { response })
       return store.dispatch(tokenSet(response.body.token))
     })
-  }
+}
 
-  export const logout = () => {
-    cookieDelete('X-SailPoint-Token')
-    return tokenRemove()
-  }
+export const logout = () => {
+  cookieDelete('X-SailPoint-Token')
+  return tokenRemove()
+}
--- src/component/landing/index.js
+++ src/component/landing/index.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import AdminForm from '../admin-form'
+import * as util from '../../lib/util.js'
+import * as auth from '../../action/store.js'
 
 class Landing extends React.Component {
   render(){
+    let {location} = this.props
     return (
       <div className='landing'>
-        <h1>Landing</h1>
-        <AdminForm />
+        <h1>Welcome</h1>
+
+        {util.renderIf(location.pathname === '/signup',
+          <div>
+            <AdminForm onComplete={this.props.signup} />
+            <p> already have an account? </p>
+            <Link to='/login'> Login </Link>
+          </div>
+        )}
+
+        {util.renderIf(location.pathname === '/login',
+          <div>
+            <AdminForm type='login' onComplete={this.props.login} />
+            <p> dont have an account ? </p>
+            <Link to='/signup'> Signup </Link>
+          </div>
+        )}
       </div>
     )
   }
 }
 
-const mapStateToProps =(state) => ({loggedIn: !!state.token})
+const mapStateToProps = (state) => ({loggedIn: !!state.token})
+const mapDispatchToProps = (dispatch) => ({
+  signup: (user) => dispatch(auth.signup(user)),
+  login: (user) => dispatch(auth.login(user)),
+})
 
-export default connect(mapStateToProps)(Landing)
+export default connect(mapStateToProps, mapDispatchToProps)(Landing)
