8ff75d4a75f8c7c9942c2ebaab05ccbb472cf882
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-11-16 15:05:44 -0800

    Logout underway

---
+++ src/component/admin-nav/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import * as auth from '../../action/auth.js'
+
+class adminNav extends React.Component {
+
+  render() {
+    let {employee} = this.props
+    return (
+      <nav className='navbar'>
+        <ul>
+          <li><button onClick={this.props.logout}>logout</button></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  employee: state.employee,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(auth.logout()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(adminNav)
