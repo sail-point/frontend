99be76f2a9db411f5c248635eb4e438c6b22d816
Ron Barrantes <ronbarrantes@gmail.com>
2017-11-16 21:29:59 -0800

    saving some of the logic

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -25,15 +25,17 @@ class App extends React.Component {
         <BrowserRouter>
           <div>
             <Route path='*' component={Header} />
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/signup' component={Landing} />
-            <Route exact path='/login' component={Landing} />
-            <Route exact path='/admin/product' component={Product} />
-            <Route exact path='/admin/employee' component={Employee} />
-            <Route path='/employee/login' component={Keypad} />
-            <Route exact path='/employee/dashboard' render={(props) => (<Dashboard {...props} employee={employee} />)} />
-            <Route exact path='/employee/products' render={(props) => (<ProductView {...props} employee={employee} />)} />
-            <Route exact path='/employee/orders' render={(props) => (<Order {...props} employee={employee} />)} />
+            <main>
+              <Route exact path='/' component={Landing} />
+              <Route exact path='/signup' component={Landing} />
+              <Route exact path='/login' component={Landing} />
+              <Route exact path='/admin/product' component={Product} />
+              <Route exact path='/admin/employee' component={Employee} />
+              <Route path='/employee/login' component={Keypad} />
+              <Route exact path='/employee/dashboard' render={(props) => (<Dashboard {...props} employee={employee} />)} />
+              <Route exact path='/employee/products' render={(props) => (<ProductView {...props} employee={employee} />)} />
+              <Route exact path='/employee/orders' render={(props) => (<Order {...props} employee={employee} />)} />
+            </main>
           </div>
         </BrowserRouter>
       </div>
