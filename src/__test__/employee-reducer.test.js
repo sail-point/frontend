f64cfc18541201e0c2fab15c32fed04af7eb8649
KatherineHanson <katherinemariehanson@gmail.com>
2017-11-17 13:16:10 -0800

    frontend employee testing further edited

--- src/__test__/employee-reducer.test.js
+++ src/__test__/employee-reducer.test.js
@@ -1,9 +1,14 @@
 import reducer from '../reducer/employees.js'
 
 describe('businessEmployee reducer', () => {
-  test('the initial state shuld be []', () => {
+  test('the initial state should be []', () => {
     let state = reducer(undefined, {type: ''})
     expect(state).toEqual([])
+  })
+
+  test('should return the state', () => {
+    let state = reducer('hello world', {type: ''})
+    expect(state).toEqual('hello world')
   })
 
   describe('EMPLOYEE_CREATE', () => {
@@ -40,11 +45,6 @@ describe('businessEmployee reducer', () => {
         })
       }
       expect(shouldFail).toThrow('Employee requires a first name, last name, title, email, phone number, hours-per-week, salary-per-hour, and pin')
-    })
-
-    test('should return the state', () => {
-      let state = reducer('hello world', {type: ''})
-      expect(state).toEqual('hello world')
     })
   })
 
@@ -228,52 +228,21 @@ describe('businessEmployee reducer', () => {
       // expect(state[1].salaryPerHour).toBe(20)
     })
 
-    test('remove should fail with no payload', () => {
+    test('destroy should fail with no payload', () => {
       let shouldFail = () => {
         reducer(undefined, {type: 'EMPLOYEE_DESTROY'})
       }
       expect(shouldFail).toThrow('Employee required')
     })
 
-    // test('update should fail with no payload', () => {
-    //   let employees = [
-    //     {
-    //       firstName: 'sei',
-    //       lastName: 'satou',
-    //       title: 'manager',
-    //       email: 'rosagigantea@lillian.net',
-    //       phoneNumber: '206-453-2437',
-    //       hoursPerWeek: 30,
-    //       salaryPerHour: 20,
-    //       pin: '2222',
-    //       _id: 1,
-    //     },
-    //     {
-    //       firstName: 'shimako',
-    //       lastName: 'toudou',
-    //       title: 'hostess',
-    //       email: 'rosagiganteaenbouton@lillian.net',
-    //       phoneNumber: '206-453-2438',
-    //       hoursPerWeek: 15,
-    //       salaryPerHour: 15,
-    //       pin: '3333',
-    //       _id: 2,
-    //     },
-    //   ]
-    //   let action = {}
-    //   let state = reducer(employees, action)
-    //   expect(state[1].firstName).toBe('shimako')
-    //   expect(state[1].salaryPerHour).toBe(15)
-    // })
-    //
-    // test('set should fail with invalid payload', () => {
-    //   let shouldFail = () => {
-    //     reducer(undefined, {
-    //       type: 'EMPLOYEES_UPDATE',
-    //       payload: {},
-    //     })
-    //   }
-    //   expect(shouldFail()).toEqual(undefined)
-    // })
+    test('destroy should fail with invalid payload', () => {
+      let shouldFail = () => {
+        reducer(undefined, {
+          type: 'EMPLOYEE_DESTROY',
+          payload: {},
+        })
+      }
+      expect(shouldFail).toThrow('Employee requires a first name, last name, title, email, phone number, hours-per-week, salary-per-hour, and pin')
+    })
   })
 })
