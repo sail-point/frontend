559f379879809c8cdc4ef66bf39a179d78abfc4f
Christina <penssake@gmail.com>
2017-11-16 21:16:19 -0800

    merge commit

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -27,22 +27,21 @@ class App extends React.Component {
             {util.renderIf(loggedIn,
               <AdminNav />
             )}
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/signup' component={Landing} />
-            <Route exact path='/login' component={Landing} />
-            <Route exact path='/admin/product' component={Product} />
-            <Route exact path='/admin/employee' component={Employee} />
-          </div>
-        </BrowserRouter>
-        <BrowserRouter>
-          <div>
-            {util.renderIf(employee,
-              <Header employee={employee} />
-            )}
-            <Route path='/employee/login' component={Keypad} />
-            <Route exact path='/employee/dashboard' render={(props) => (<Dashboard {...props} employee={employee} />)} />
-            <Route exact path='/employee/products' render={(props) => (<ProductView {...props} employee={employee} />)} />
-            <Route exact path='/employee/orders' render={(props) => (<Order {...props} employee={employee} />)} />
+            <Header />
+            <main>
+              <Route exact path='/' component={Landing} />
+              <Route exact path='/signup' component={Landing} />
+              <Route exact path='/login' component={Landing} />
+              <Route exact path='/admin/product' component={Product} />
+              <Route exact path='/admin/employee' component={Employee} />
+              {util.renderIf(employee,
+                <Header employee={employee} />
+              )}
+              <Route path='/employee/login' component={Keypad} />
+              <Route exact path='/employee/dashboard' render={(props) => (<Dashboard {...props} employee={employee} />)} />
+              <Route exact path='/employee/products' render={(props) => (<ProductView {...props} employee={employee} />)} />
+              <Route exact path='/employee/orders' render={(props) => (<Order {...props} employee={employee} />)} />
+            </main>
           </div>
         </BrowserRouter>
       </div>
