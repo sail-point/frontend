307e1de0d11372cae91cd5cf67698620b91d618b
Ron Barrantes <ronbarrantes@gmail.com>
2017-11-17 01:28:43 -0800

    working on the css

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,61 +1,59 @@
 import './header.scss'
 import React from 'react'
+import { log } from 'util'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import * as util from '../../lib/util.js'
 import * as auth from '../../action/auth.js'
-import { renderIf } from '../../lib/util.js';
-import { log } from 'util';
+import { renderIf } from '../../lib/util.js'
 
 class Header extends React.Component {
 
   render() {
     let { employee, loggedIn } = this.props
-    console.log('-->', this.props)
 
     return (
       <header>
+        <div>
+          <h1><Link to='/'>Sail Point</Link></h1>
+          {util.renderIf(!employee && !loggedIn,
+            <nav className='navbar'>
+              <ul className='signingInSection'>
+                <li><Link to='/login'>Log In</Link></li>
+                <li><Link to='/signup'>Sign Up</Link></li>
+              </ul>
+            </nav>
+          )}
 
-        <h1>Sail Point</h1>
-        {util.renderIf(!employee && !loggedIn,
-          <nav className='navbar'>
-            <ul>
-              <li>Not logged in</li>
-              <li><Link to='/login'>Log In</Link></li>
-              <li><Link to='/signup'>Sign Up</Link></li>
-            </ul>
-          </nav>
-        )}
+          {util.renderIf(loggedIn && !employee,
+            <nav className='navbar'>
+              <ul>
+                <li><Link to='/admin/employee'>Employee</Link></li>
+                <li><Link to='/admin/product'>Product</Link></li>
+                <li><Link to='/employee/login'>Employee Login</Link></li>
+              </ul>
 
-        {util.renderIf(loggedIn && !employee,
-          <nav className='navbar'>
-            <ul>
-              <li>loggedIn not employee</li>
-              <li><Link to='/admin/employee'>Employee</Link></li>
-              <li><Link to='/admin/product'>Product</Link></li>
-            </ul>
+              <ul className='signingInSection'>
+                <li></li>
+              </ul>
+              <button className='logout-btn' onClick={this.props.logout}>logout</button>
+            </nav>
+          )}
+          {util.renderIf(loggedIn && employee,
+            <nav className='navbar'>
+              <ul>
+                <li><Link to='/employee/products'>Products</Link></li>
+                <li><Link to='/employee/employees'>Employees</Link></li>
+                <li><Link to='/employee/orders'>Orders</Link></li>
+                <li><Link to='/admin/product'>Back To admin</Link></li>
+              </ul>
 
-            <ul>
-              <li><button onClick={this.props.logout}>logout</button></li>
-              <li><Link to='/employee/login'>Employee Login</Link></li>
-            </ul>
-          </nav>
-        )}
-        {util.renderIf(loggedIn && employee,
-          <nav className='navbar'>
-            <ul>
-              <li>loggedIn Employee</li>
-              <li><Link to='/employee/products'>Products</Link></li>
-              <li><Link to='/employee/employees'>Employees</Link></li>
-              <li><Link to='/employee/orders'>Orders</Link></li>
-            </ul>
-
-            <ul>
-              <li><button onClick={this.props.logout}>logout</button></li>
-              <li><Link to='/employee/login'>Employee Login</Link></li>
-            </ul>
-          </nav>
-        )}
+              <ul className='signingInSection'>
+                <li>Clock</li>
+              </ul>
+            </nav>
+          )}
+        </div>
       </header>
     )
   }
